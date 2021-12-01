dd043641c2064dc69e5662a51ff5a9ef1c6519ca
Gabriela <gabriela.azevedo12@gmail.com>
2021-12-01 15:18:24 -0300

    Creating on memory users

--- src/main/java/com/eazybytes/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.eazybytes.springsecuritybasic.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,5 +18,12 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/notices").permitAll()
                 .antMatchers("/contact").permitAll()
                 .and().formLogin().and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin").and()
+                .withUser("user").password("12345").authorities("read").and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
