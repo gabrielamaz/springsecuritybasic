b29a235135b99dd28f63a81e8f7bab300c310a72
Gabriela <gabriela.azevedo12@gmail.com>
2021-12-07 14:18:28 -0300

    Implementing Custom UserDetails

--- src/main/java/com/eazybytes/springsecuritybasic/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -4,12 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -42,10 +38,10 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsManager);
     }*/
 
-    @Bean
+    /*@Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
-    }
+    }*/
 
     @Bean
     public PasswordEncoder passwordEncoder() {
